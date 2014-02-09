adee6273cd2040df5da46682f29440d0133b2cac
Piotr Niziniecki <pniziniecki@gmail.com>
2014-02-09 17:23:07 +0100

    configuration mock

---
+++ js/services/configuration.js
@@ -0,0 +1,14 @@
+'use strict';
+
+cloudScrum.service('Configuration', function Configuration() {
+
+    var self = this;
+
+    self.getStoriesStatuses = function() {
+        return ['Not started', 'In progress', 'Completed', 'Testing', 'Blocked', 'Accepted']; //TODO grab from google drive?
+    };
+
+    self.getTasksStatuses = function() {
+        return ['', 'In progress', 'Blocked', 'Completed']; //TODO grab from google drive?
+    };
+});
